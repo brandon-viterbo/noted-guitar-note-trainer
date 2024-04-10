8a0ba08313d1e33c7a528508b2e7d2c403c57ef4
brandon-viterbo <brandonjviterbo@gmail.com>
2024-04-10 18:06:58 -0400

    Added eslint and changed JS script to comply with Airbnb style guide

--- index.js
+++ index.js
@@ -1,80 +1,85 @@
-window.onload = (event) => { 
-  generateNoOfStringsOptions();
-}
+function generateNoOfStringsOptions() {
+  const currentStrings = document.getElementById('currentStrings');
+  const min = 4;
+  const max = 8;
+  const defaultNo = 6;
+  const select = document.getElementById('submittedStrings');
+  let i;
 
-function generateNoOfStringsOptions (){
-  let currentStrings = document.getElementById("currentStrings"),
-    min = 4,
-    max = 8,
-    defaultNo = 6,
-    select = document.getElementById('submittedStrings');
-
-  for (var i = min; i<=max; i++){
-    var opt = document.createElement('option');
+  for (i = min; i <= max; i += 1) {
+    const opt = document.createElement('option');
     opt.value = i;
     opt.innerHTML = i;
-    if (i === defaultNo) { 
-      opt.selected = "selected"; 
+    if (i === defaultNo) {
+      opt.selected = 'selected';
       currentStrings.innerHTML = i;
     }
     select.appendChild(opt);
   }
 }
 
-let settingsForm = document.getElementById("settingsForm");
-settingsForm.addEventListener("submit", (e) => {
-  console.log("Update request submitted...")
-  e.preventDefault();
-  let currentTuningElem = document.getElementById("currentTuning"),
-    currentStringsElem = document.getElementById("currentStrings"),
-    submittedTuningStr = document.getElementById("customTuning").value,
-    submittedStringsStr = document.getElementById("submittedStrings").value;
+window.onload = () => {
+  generateNoOfStringsOptions();
+};
 
-  updateGuitar(currentTuningElem, currentStringsElem, submittedTuningStr, submittedStringsStr);
-})
+async function validateTuning(submittedTuningStr, submittedStringsStr) {
+  /* const response = await fetch('./notes.json'),
+    notes = await response.text(),
+    parsedText = await JSON.parse(notes);
 
-async function updateGuitar(currentTuningElem, currentStringsElem, submittedTuningStr, submittedStringsStr) {
-  if (await validateSettings(submittedTuningStr, submittedStringsStr)) {
-    currentTuningElem.innerHTML = submittedTuningStr;
-    currentStringsElem.innerHTML = submittedStringsStr;
-    console.log('Settings updated.')
+  console.log(parsedText['notes'][0]);
+  console.log(parsedText['notes'].includes("A")); */
+
+  // tuningRegex matches a sequence of x valid notes, where x is the number of
+  // strings on the user's guitar
+  // const tuningRegex = new RegExp('^([A-G](b|#)?)\{'+submittedStringsStr+'\}$');
+  const tuningRegex = new RegExp(`^([A-G](b|#)?){${submittedStringsStr}}$`);
+  const tuningStr = submittedTuningStr.replace(/\s + /g, '');
+
+  const valid = tuningRegex.test(tuningStr);
+
+  if (valid) {
+    console.log(`${tuningStr} is a valid tuning`);
   } else {
-    console.log('ERROR: Cannot update settings.')
+    console.log(`${tuningStr} is an valid tuning`);
   }
+
+  return valid;
 }
 
 async function validateSettings(submittedTuningStr, submittedStringsStr) {
-  let valid = await validateTuning(submittedTuningStr, submittedStringsStr);
+  const valid = await validateTuning(submittedTuningStr, submittedStringsStr);
 
   if (valid) {
-    console.log('All settings are valid.'); 
+    console.log('All settings are valid.');
   } else {
     console.log('ERROR: At least one setting is invalid.');
   }
-  
-  return valid
-}
 
-async function validateTuning(submittedTuningStr, submittedStringsStr) {
-  //const response = await fetch('./notes.json'),
-  //  notes = await response.text(),
-  //  parsedText = await JSON.parse(notes);
-
-  //console.log(parsedText['notes'][0]);
-  //console.log(parsedText['notes'].includes("A"));
-
-  //tuningRegex matches a sequence of x valid notes, where x is the number of 
-  //strings on the user's guitar
-  let tuningRegex = new RegExp('^([A-G](b|#)?)\{'+submittedStringsStr+'\}$'),
-    tuningStr = submittedTuningStr.replace(/\s+/g, '');
-
-  let valid = tuningRegex.test(tuningStr);
-
-  if (valid) {
-    console.log(tuningStr + ' is a valid tuning')
-  } else {
-    console.log(tuningStr + ' is an invalid tuning')
-  }
-  
   return valid;
 }
+
+async function updateGuitar(currTuningElem, currStringsElem, submitTuningStr, submitStringsStr) {
+  const updatedTuning = currTuningElem;
+  const updatedStrings = currStringsElem;
+
+  if (await validateSettings(submitTuningStr, submitStringsStr)) {
+    updatedTuning.innerHTML = submitTuningStr;
+    updatedStrings.innerHTML = submitStringsStr;
+    console.log('Settings updated.');
+  } else {
+    console.log('ERROR: Cannot update settings.');
+  }
+}
+
+const settingsForm = document.getElementById('settingsForm');
+settingsForm.addEventListener('submit', (e) => {
+  console.log('Update request submitted...');
+  e.preventDefault();
+  const currentTuningElem = document.getElementById('currentTuning');
+  const currentStringsElem = document.getElementById('currentStrings');
+  const submittedTuningStr = document.getElementById('customTuning').value;
+  const submittedStringsStr = document.getElementById('submittedStrings').value;
+
+  updateGuitar(currentTuningElem, currentStringsElem, submittedTuningStr, submittedStringsStr);
+});
