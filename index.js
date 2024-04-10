2fc58dd1330bd3284809deb425d07d239266a2b4
brandon-viterbo <brandonjviterbo@gmail.com>
2024-04-10 15:01:46 -0400

    Removed global variables, and created updateGuitar(). Validate functions now only take input and return boolean and don't update HTML.

--- index.js
+++ index.js
@@ -25,51 +25,48 @@ let settingsForm = document.getElementById("settingsForm");
 settingsForm.addEventListener("submit", (e) => {
   console.log("Update request submitted...")
   e.preventDefault();
+  let currentTuningElem = document.getElementById("currentTuning"),
+    currentStringsElem = document.getElementById("currentStrings"),
+    submittedTuningStr = document.getElementById("customTuning").value,
+    submittedStringsStr = document.getElementById("submittedStrings").value;
 
-  updateGuitar()
+  updateGuitar(currentTuningElem, currentStringsElem, submittedTuningStr, submittedStringsStr);
 })
 
-async function updateGuitar() {
-  let currentTuning = document.getElementById("currentTuning"),
-    currentStrings = document.getElementById("currentStrings"),
-    submittedTuning = document.getElementById("customTuning").value,
-    submittedStrings = document.getElementById("submittedStrings").value;
-
-  if (await validateSettings()) {
-    currentTuning.innerHTML = submittedTuning;
-    currentStrings.innerHTML = submittedStrings;
+async function updateGuitar(currentTuningElem, currentStringsElem, submittedTuningStr, submittedStringsStr) {
+  if (await validateSettings(submittedTuningStr, submittedStringsStr)) {
+    currentTuningElem.innerHTML = submittedTuningStr;
+    currentStringsElem.innerHTML = submittedStringsStr;
+    console.log('Settings updated.')
   } else {
-    console.log('Cannot update.')
+    console.log('ERROR: Cannot update settings.')
   }
 }
 
-async function validateSettings() {
-  let valid = await validateTuning();
+async function validateSettings(submittedTuningStr, submittedStringsStr) {
+  let valid = await validateTuning(submittedTuningStr, submittedStringsStr);
 
   if (valid) {
-    console.log('All settings good'); 
+    console.log('All settings are valid.'); 
   } else {
-    console.log('At least one setting is bad');
+    console.log('ERROR: At least one setting is invalid.');
   }
   
   return valid
 }
 
-async function validateTuning() {
-  const response = await fetch('./notes.json'),
-    notes = await response.text(),
-    parsedText = await JSON.parse(notes);
+async function validateTuning(submittedTuningStr, submittedStringsStr) {
+  //const response = await fetch('./notes.json'),
+  //  notes = await response.text(),
+  //  parsedText = await JSON.parse(notes);
 
   //console.log(parsedText['notes'][0]);
   //console.log(parsedText['notes'].includes("A"));
 
-  let submittedTuning = document.getElementById("customTuning").value,
-    submittedStrings = document.getElementById("submittedStrings").value;
-
-  /*tuningRegex matches a sequence of x valid notes, where x is the number of 
-  strings on the user's guitar*/
-  let tuningRegex = new RegExp('^([A-G](b|#)?)\{'+submittedStrings+'\}$'),
-    tuningStr = submittedTuning.replace(/\s+/g, '');
+  //tuningRegex matches a sequence of x valid notes, where x is the number of 
+  //strings on the user's guitar
+  let tuningRegex = new RegExp('^([A-G](b|#)?)\{'+submittedStringsStr+'\}$'),
+    tuningStr = submittedTuningStr.replace(/\s+/g, '');
 
   let valid = tuningRegex.test(tuningStr);
 
