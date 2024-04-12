7c78e22a35cb35e8100570ae028872a7c5c27f63
brandon-viterbo <brandonjviterbo@gmail.com>
2024-04-12 15:38:01 -0400

    Added test loop. TODO: Let user answer questions.

--- index.js
+++ index.js
@@ -28,36 +28,6 @@ function validateSettings(submittedTuningStr, submittedStringsStr) {
   return valid;
 }
 
-const twelveNotes = [
-  'A', 'A',
-  'A#', 'Bb',
-  'B', 'Cb',
-  'C', 'B#',
-  'C#', 'Db',
-  'D', 'D',
-  'D#', 'Eb',
-  'E', 'Fb',
-  'F', 'E#',
-  'F#', 'Gb',
-  'G', 'G',
-  'G#', 'Ab',
-];
-
-const defaultStringsNo = 6;
-const noOffrets = 24;
-
-const guitarModel = {
-  tuningStr: 'EADGBE',
-  frets: noOffrets,
-  strings: defaultStringsNo,
-  fretboard: null,
-  updateView(viewElementID) {
-    const view = document.getElementById(viewElementID);
-
-    view.innerHTML = `Tuning is ${guitarModel.tuningStr}. Number of strings is ${guitarModel.strings}`;
-  },
-};
-
 function changeGuitarModel(guitar, allNotes, newTuning, newStrings) {
   let i;
   let j;
@@ -122,12 +92,43 @@ function generateNoOfStringsOptions(defaultNo) {
   }
 }
 
+const twelveNotes = [
+  'A', 'A',
+  'A#', 'Bb',
+  'B', 'Cb',
+  'C', 'B#',
+  'C#', 'Db',
+  'D', 'D',
+  'D#', 'Eb',
+  'E', 'Fb',
+  'F', 'E#',
+  'F#', 'Gb',
+  'G', 'G',
+  'G#', 'Ab',
+];
+
+const defaultStringsNo = 6;
+const noOffrets = 24;
+
+const guitarModel = {
+  tuningStr: 'EADGBE',
+  frets: noOffrets,
+  strings: defaultStringsNo,
+  fretboard: null,
+  updateView(viewElementID) {
+    const view = document.getElementById(viewElementID);
+
+    view.innerHTML = `Tuning is ${guitarModel.tuningStr}. Number of strings is ${guitarModel.strings}`;
+  },
+};
+
 const viewElementID = 'guitarInfo';
 
 window.onload = () => {
   generateNoOfStringsOptions(guitarModel.strings);
-  guitarModel.updateView(viewElementID);
 };
+changeGuitarModel(guitarModel, twelveNotes, 'EADGBE', 6);
+guitarModel.updateView(viewElementID);
 
 // MVC pattern here. settingsForm is the controller...
 const settingsForm = document.getElementById('settingsForm');
@@ -143,9 +144,69 @@ settingsForm.addEventListener('submit', (e) => {
   console.log(guitarModel.fretboard);
 });
 
-const testForm = document.getElementById('testOptions');
+function makeQuestionAndAnswer(guitar, minString, maxString, minFret, maxFret, selectedMode) {
+  let foundErr = true;
+  let questionAndAnswer;
+
+  function helper() {
+    const string = Math.floor(Math.random() * maxString) + minString;
+    const fret = Math.floor(Math.random() * maxFret) + minFret;
+    const note = guitar.fretboard[string - 1][fret];
+    // return array in form of [question, answer]
+    if (selectedMode === 'identify') {
+      return [`String ${string}, fret ${fret} is the note...`, note];
+    } if (selectedMode === 'locate') {
+      return [`The note ${note} is located on...`, [string, fret]];
+    }
+    throw new Error('ERROR: Selected Training Mode is not valid!');
+  }
+
+  while (foundErr) {
+    try {
+      questionAndAnswer = helper(guitar, minString, maxString, minFret, maxFret, selectedMode);
+      foundErr = false;
+    } catch (err) {
+      questionAndAnswer = -1;
+    }
+  }
+
+  return questionAndAnswer;
+}
+
+const testOptions = document.getElementById('testOptions');
+const noOfQuestionsElement = document.getElementById('noOfQuestions');
+const submitAnswerButton = document.createElement('button');
 const testPrompt = document.getElementById('testPrompt');
-testForm.addEventListener('submit', (e) => {
+const testInputs = document.getElementById('testInputs');
+let questionAndAnswer = makeQuestionAndAnswer(guitarModel, 0, guitarModel.strings, 0, guitarModel.frets, 'identify');
+let question = questionAndAnswer[0];
+let answer = questionAndAnswer[1];
+let questionsToAnswer;
+
+console.log(guitarModel.fretboard);
+console.log(`Must answer for question ${question}`);
+
+submitAnswerButton.type = 'submit';
+submitAnswerButton.innerHTML = 'Submit Answer';
+submitAnswerButton.value = 'submitAnswer';
+testOptions.addEventListener('submit', (e) => {
   e.preventDefault();
-  testPrompt.innerHTML = 'Test started!';
+  testInputs.appendChild(submitAnswerButton);
+  questionsToAnswer = noOfQuestionsElement.value;
+  testPrompt.innerHTML = question;
+});
+
+testInputs.addEventListener('submit', (e) => {
+  console.log(`Submitted answer for question ${question}`);
+  e.preventDefault();
+  questionsToAnswer -= 1;
+  if (questionsToAnswer === 0) {
+    testPrompt.innerHTML = 'Test done! Your score is: ';
+    testInputs.removeChild(submitAnswerButton);
+    return;
+  }
+  questionAndAnswer = makeQuestionAndAnswer(guitarModel, 0, guitarModel.strings, 0, guitarModel.frets, 'identify');
+  [question, answer] = questionAndAnswer;
+  testPrompt.innerHTML = question;
+  console.log(`Must answer for question ${question}`);
 });
